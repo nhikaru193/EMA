ada0412aa4565515eb2fc4489f16798ecb56af10
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-23 17:44:13 +0900

    Update motor.py

--- motor.py
+++ motor.py
@@ -116,12 +116,12 @@ class MotorDriver():
 	 time.sleep(0.01)
 	 
      def changing_Lforward(self, before, after):
-	 	 global speed
-		 for i in range(1, 200):
-			 delta_speed = (after - before) / 200
-			 speed = before + i * delta_speed
-			 self.motor_Lforward(speed)
-			 time.sleep(0.01)
+	 global speed
+	 for i in range(1, 200):
+		 delta_speed = (after - before) / 200
+		 speed = before + i * delta_speed
+		 self.motor_Lforward(speed)
+		 time.sleep(0.01)
  
 def changing_Rforward(self, before, after):
 	 global speed
