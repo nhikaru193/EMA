3ddb7362c6fd3992e07ec2083290bb4d0beb853d
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-23 12:21:34 +0900

    Update test_motor_turning.py

--- test_motor_turning.py
+++ test_motor_turning.py
@@ -40,7 +40,7 @@ bno = BNO055()
 if bno.begin() is not True:
         print("Error initializing device")
         exit()
-    time.sleep(1)
+        time.sleep(1)
 bno.setMode(BNO055.OPERATION_MODE_NDOF)
 bno.setExternalCrystalUse(True)
 time.sleep(1)
