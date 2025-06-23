cfafc5b456b1fa6fb9f0396173004b42699852bb
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-23 17:05:13 +0900

    Update EM_Goal_Detective_NOSHIRO.py

--- EM_Goal_Detective_NOSHIRO.py
+++ EM_Goal_Detective_NOSHIRO.py
@@ -20,62 +20,93 @@ Va = 0
 Vb = 0
 
 try:
+    
+    print("対象物を画面内に収める")
+    #画面内に映っていない場合に探す
     while True:
-        #関数定義
         percentage = color.get_percentage()
-        
-        # 判定出力
-        print(f"🔴 赤割合: {percentage:.2f}% → ", end="")
 
-        #画面場所検知
+        if percentage > 5:
+            break
+
+        elif:
+            driver.changing_right(0, 60)
+            driver.changing_right(60, 0)
+
+    
+    print("対象物を画面中央に収める")
+    #画面中央(横に五分割した中央)に収める
+    while true:
         number = color.get_block_number()
         
-        if percentage >= 10.0:
-             Vb = 0
-             print("非常に近い（終了）")
-             driver.changing_forward(Va, Vb)
-             driver.motor_stop_brake()
-             break
-          
-        elif percentage >= 5.0:
-             Vb = 50
-             print("近い")
-             driver.changing_forward(Va, Vb)
-             time.sleep(0.1)
-             Va = Vb
-          
-        elif percentage >= 2.0:
-             Vb = 100
-             print("遠い")
-             driver.changing_forward(Va, Vb)
-             time.sleep(0.1)
-             Va = Vb
+        if number == 1:
+            driver.changing_left(0, 40)
+            driver.changing_left(40, 0)
 
-        else: 
-            print("範囲外")
-            while True:
-                driver.changing_forward(Va, 0)
-                driver.motor_stop_brake()
+        elif number == 2:
+            driver.changing_left(0, 25)
+            driver.changing_left(25, 0)
 
-                if number == 1:
-                    driver.changing_left(0, 15)
-                    driver.changing_left(15, 0)
+        elif number == 3:
+            break
+        
+        elif number == 4:
+            driver.changing_right(0, 25)
+            driver.changing_right(25, 0)
+            
+        else:
+            driver.changing_right(0, 40)
+            driver.changing_right(40, 0)
+    
 
-                elif number == 5:
-                    driver.changing_right(0, 15)
-                    driver.changing_right(15, 0)
+    #画面中央に写してからの誘導(画面外へ出ることはないと想定)
+    while true:
+        #画面割合、場所検知
+        percentage = color.get_percentage()
+        number = color.get_block_number()
+        
+        # 判定出力
+        print(f"赤割合: {percentage:2f}%-----画面場所:{number}です ")
+
+        if number == 3:
+            if percentage > 40:
+                break
+
+            elif percentage > 20:
+                Vb = 40
+                driver.changing_forward(Va, Vb)
+                Va = Vb
+
+            elif percentage > 10:
+                Vb = 60
+                driver.changing_forward(Va, Vb)
+                Va = Vb
+
+            else:
+                Vb = 80
+                driver.changing_forward(Va, Vb)
+                Va = Vb
+
+        elif number == 1:
+            driver.changing_left(0, 40)
+            driver.changing_left(40, 0)
+
+        elif number == 2:
+            driver.changing_left(0, 25)
+            driver.changing_left(25, 0)
+        
+        elif number == 4:
+            driver.changing_right(0, 25)
+            driver.changing_right(25, 0)
+            
+        elif number == 5:
+            driver.changing_right(0, 40)
+            driver.changing_right(40, 0)
                 
-                #割合取得
-                percentage = color.get_percentage()
-                
-                if percentage >= 2.0:
-                   Vb = 50
-                   print("遠い")
-                   driver.changing_forward(Va, Vb)
-                   Va = Vb
-                   break               
-                  
+
 finally:
     picam2.close()
-    print("カメラを閉じました。プログラム終了。")
+    print("カメラを閉じました。")
     print("ゴール判定")
+    driver.cleanup()
+    print("GPIOクリーンアップが終了しました。プログラムを終了します")
