817f7f0a5a51bc4a28c5de07f8f4864e143bb6f7
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-23 17:46:31 +0900

    Update motor.py

--- motor.py
+++ motor.py
@@ -97,10 +97,10 @@ class MotorDriver():
          self.pwmb.ChangeDutyCycle(speed)
 		 
      def motor_Lforward(self, speed):
-	 GPIO.output(self.A1, GPIO.LOW)
-	 GPIO.output(self.A2, GPIO.HIGH)
-	 self.pwma.ChangeDutyCycle(speed) 
-	     
+		 GPIO.output(self.A1, GPIO.LOW)
+		 GPIO.output(self.A2, GPIO.HIGH)
+		 self.pwma.ChangeDutyCycle(speed) 
+			 
      def motor_Rforward(self, speed):
 	 GPIO.output(self.B1, GPIO.HIGH)
 	 GPIO.output(self.B2, GPIO.LOW)
