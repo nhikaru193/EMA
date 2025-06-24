018d711564d76335ee71afcb4a91e6035d22fbbf
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-24 17:35:07 +0900

    Update test_motor_turning.py

--- test_motor_turning.py
+++ test_motor_turning.py
@@ -37,7 +37,7 @@ driver = MotorDriver(
 #BNO055のインスタンス作成
 bno = BNO055()
 while True:
-    sys, gyro, accel, mag = bno.getcalibration()
+    sys, gyro, accel, mag = bno.get_calibration()
     if gyro == 3:
         break
 if bno.begin() is not True:
