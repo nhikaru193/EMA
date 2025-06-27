ee8cc7a1a9e070b060089fe862a622111d43b77d
gitogitRyotaro <morohashi@kimura-lab.net>
2025-06-27 19:43:41 +0900

    Update EM_Landing_Inpact.py

--- EM_Landing_Inpact.py
+++ EM_Landing_Inpact.py
@@ -3,6 +3,49 @@ import time
 import struct
 from BNO055 import BNO055
 import math
+import BME280
+import BNO055
 
-while True:
-    
+print("bme280の動作確認中")
+time.sleep(1)
+print("bme280の動作確認中")
+time.sleep(1)
+print("bme280の動作確認中")
+
+#bme280の初期設定
+# BME280関連のグローバル変数
+t_fine = 0.0
+digT = []
+digP = []
+digH = []
+i2c = smbus.SMBus(1)
+address = 0x76 
+BME280.init_bme280()
+BME280.read_compensate()
+
+for i range (100):
+    BME280.read_data()
+	time.sleep(0.02)
+
+print("bme280の動作終了中")
+time.sleep(1)
+print("bme280の動作終了中")
+time.sleep(1)
+print("bme280の動作終了中")
+
+
+print("BNO055の動作確認中")
+time.sleep(1)
+print("BNO055の動作確認中")
+time.sleep(1)
+print("BNO055の動作確認中")
+
+#BNO055の初期設定
+bno = BNO055()
+time.sleep(1)
+bno.begin()
+time.sleep(1)
+bno.setmode(BNO055.OPERATION_MODE_NDOF)
+bno.setExternalCrystalUse(True)
+time.sleep(1)
+
