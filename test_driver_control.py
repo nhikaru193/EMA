c87cc2e8269415ddbd8b2954aab6c1cab35d3df1
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-06 11:49:23 +0900

    Update test_driver_control.py

--- test_driver_control.py
+++ test_driver_control.py
@@ -1,30 +1,28 @@
 import RPi.GPIO as GPIO
 import time
 from motor import MotorDriver
+from BNO055 import BNO055
+import smbus
+import struct
 
+#モータードライバのインスタンス作成
 driver = MotorDriver(
     PWMA=12, AIN1=23, AIN2=18,   # 左モーター用（モータA）
     PWMB=19, BIN1=16, BIN2=26,   # 右モーター用（モータB）
     STBY=21                      # STBYピン
 )
 
-try:
-    """
-    for i in range (15):
-        L = 85
-        k = L - i
-        print(f"duty比{k}です")
-        driver.changing_moving_forward(0, L, 0, k)
-        print("準備完了です")
-        time.sleep(4)
-        print("減速を開始します")
-        driver.changing_moving_forward(L, 0, k, 0)
-        time.sleep(0.2)
-    """
-    driver.changing_forward(0, 100)
-    time.sleep(3)
-    driver.changing_forward(100, 0)
+#九軸センサのインスタンス作成
+bno = BNO055()
+bno.begin()
+time.sleep(1)
+bno.setMode(BNO055.OPERATION_MODE_NDOF)
+time.sleep(1)
+bno.setExternalCrystalUse(True)
 
+try:
     
+    
+
 finally:
     driver.cleanup()
