2f1fc9f95585abf0ddf795395c13e560fb76daad
nhikaru193 <nakagawa@kimura-lab.net>
2025-07-07 16:43:46 +0900

    Update EM_excellent_gps.py

--- EM_excellent_gps.py
+++ EM_excellent_gps.py
@@ -179,36 +179,7 @@ def navigate_to_goal():
 
             # 5. 前進フェーズ (PD制御による直進維持)
             print(f"[MOVE] 方向OK。PD制御で前進します。")
-            def follow_forward(driver, bno, base_speed, duration_time):
-                target = bno.get_heading()
-    
-                #パラメータ
-                base_speed = base_speed
-                Kp = 0.80
-                Kd = 0
-                loop_interval = 0.10
-                prev_err = 0.0
-                derr = 0
-                ls = 0
-                rs = 0
-                start_time = time.time()
-                driver.changing_forward(0, base_speed)
-
-                try:
-                    while True:
-                        current = bno.get_heading()
-                        err = (current - target + 180) % 360 - 180
-                        correction = Kp * err + Kd * derr
-                        ls = max(0, min(100, base_speed - correction))
-                        rs = max(0, min(100, base_speed + correction))
-                        driver.motor_Lforward(ls)
-                        driver.motor_Rforward(rs)
-                        after = bno.get_heading()
-                        time.sleep(loop_interval)
-                        derr = (after - current) / loop_interval
-                        delta_time = time.time() - start_time
-                        if delta_time > duration_time:
-                            driver.motor_stop_free()  
+            following.follow_forward(driver, bno, 70, 8)
 
     except KeyboardInterrupt:
         print("\n[STOP] 手動で停止されました。")
