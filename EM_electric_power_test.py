550452e0db31ea407dfa5de67e609ec8c4444d44
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-07 19:55:33 +0900

    Update EM_electric_power_test.py

--- EM_electric_power_test.py
+++ EM_electric_power_test.py
@@ -9,20 +9,23 @@ driver = MotorDriver(
     STBY=21
 )
 try:
-    
-    driver.changing_forward(0, 90)
     start_time = time.time()
-    while True:
+    while time.time() - start_time < 14000:
+        driver.changing_forward(0, 90)
         elapsed = int(time.time() - start_time)
         hours = elapsed // 3600
         minutes = (elapsed % 3600) // 60
         seconds = elapsed % 60
-        
         print(f"現在の経過時間は{hours}時間{minutes}分{seconds}秒です")
-
-        if elapsed > 14000:
-            break
-
+        time.sleep(30)
+        driver.changing_forward(90, 0)
+        time.sleep(5)
+        driver.changing_right(0, 40)
+        driver.changing_right(40, 0)
+        driver.changing_left(0, 40)
+        driver.changing_left(40, 0)
+        time.sleep(5)
+    time.sleep(2)
 except keyboardInterrupt:
     print("計測を終了します")
 
