7abe867fb5cb66de2c3c82e853558a0e4437c2ee
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-07 19:23:43 +0900

    Create EM_electric_power_test.py

---
+++ EM_electric_power_test.py
@@ -0,0 +1,26 @@
+import time
+import motor
+import RPi.GPIO as GPIO
+
+# === モーターインスタンス作成===
+driver = MotorDriver(
+    PWMA=12, AIN1=23, AIN2=18,    # 左モーター
+    PWMB=19, BIN1=16, BIN2=26,    # 右モーター
+    STBY=21
+)
+try:
+    driver.changing_forward(0, 90)
+    start_time = time.time()
+    
+    elapsed = int(time.time() - start_time)
+    hours = elapsed // 3600
+    minutes = (elapsed % 3600) // 60
+    seconds = elapsed % 60
+    
+    print(f"現在の経過時間は{hours}時間{minutes}分{seconds}秒です")
+
+except keyboardInterrupt:
+    print("計測を終了します")
+
+finally:
+    driver.cleanup()
