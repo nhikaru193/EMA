49a3abf6061fcd7dd61b40343174f230588e40f8
garigari-hota <daoyuanfanang@gmail.com>
2025-07-08 15:34:47 +0900

    Update parakai.py

--- parakai.py
+++ parakai.py
@@ -34,8 +34,8 @@ class BNO055Wrapper:
         return heading
 
 # --- 定数設定 (変更なし) ---
-destination_lat = 35.9185366
-destination_lon = 139.9085042
+destination_lat = 35.9189577
+destination_lon = 139.9074353
 RX_PIN = 17
 
 # --- 関数定義 (省略 - 変更なし) ---
