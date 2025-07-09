ecf307611399c69c863fcab278c2c9b3f690eb62
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-09 13:39:44 +0900

    Update following.py

--- following.py
+++ following.py
@@ -44,6 +44,48 @@ def follow_forward(driver, bno, base_speed, duration_time):
                     driver.motor_Rforward(rs)
                     time.sleep(0.03)
                 break
-
     finally:
         print("誘導終了")
+
+#petit_forward0.1秒所要、減速0.2秒所要、
+def follow_petit_forward(driver, bno, base_speed, duration_time):
+        target = bno.get_heading()
+    
+    #パラメータ
+    base_speed = base_speed
+    Kp = 0.80
+    Kd = 0
+    loop_interval = 0.10
+    prev_err = 0.0
+    derr = 0
+    ls = 0
+    rs = 0
+    start_time = time.time()
+    driver.petit_forward(0, base_speed)
+
+    try:
+        while True:
+            current = bno.get_heading()
+            err = (current - target + 180) % 360 - 180
+            correction = Kp * err + Kd * derr
+            ls = max(0, min(100, base_speed - correction))
+            rs = max(0, min(100, base_speed + correction))
+            driver.motor_Lforward(ls)
+            driver.motor_Rforward(rs)
+            after = bno.get_heading()
+            time.sleep(loop_interval)
+            derr = (after - current) / loop_interval
+            delta_time = time.time() - start_time
+            if delta_time > duration_time:
+                for i in range (1, 20):
+                    d_ls = ls / 20
+                    d_rs = rs / 20
+                    ls = ls - i * d_ls
+                    rs = rs - i * d_rs
+                    driver.motor_Lforward(ls)
+                    driver.motor_Rforward(rs)
+                    time.sleep(0.01)
+                break
+    finally:
+        print("誘導終了")
+    
