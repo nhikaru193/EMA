d83e8c1d8629da00fe82241126341336eac85992
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-09 10:38:19 +0900

    Update test_motor_turning.py

--- test_motor_turning.py
+++ test_motor_turning.py
@@ -28,6 +28,21 @@ def test_turning(duty):
     print(f"平均角速度は{average} 度/s です")
     time.sleep(2)
 
+def test_turning(duty):
+    print(f"デューティ比{duty}まで加速中です..")
+    driver.changing_right(0, duty)
+    print(f"デューティ比{duty}まで加速完了 + 角変化量測定を開始します")
+    Departure_point = bno.get_heading()
+    time.sleep(1.2)
+    print("角変化量計測終了 + 減速を開始します")
+    Arrival_point = bno.get_heading()
+    driver.changing_right(duty, 0)
+    dturn = measurement_turning(Departure_point, Arrival_point)
+    average = dturn / 1.2
+    print(f"計測終了です。デューティ比{duty}において")
+    print(f"総回転角度は{dturn} 度 です")
+    print(f"平均角速度は{average} 度/s です")
+    time.sleep(2)
 
 #モータの初期化
 driver = MotorDriver(
@@ -59,6 +74,15 @@ time.sleep(1)
 bno.setMode(BNO055.OPERATION_MODE_NDOF)
 bno.setExternalCrystalUse(True)
 time.sleep(1)
-for i in range (5, 10):
-  test_turning(10 * i)
+for i in range (1, 10):
+    speed = i * 10
+    print(f"{speed}で回転します")
+    driver.quick_left(0, speed)
+    driver.quick_left(speed, 0)
+    print("待機時間2s")
+    time.sleep(1)
+    print("待機時間1s")
+    time.sleep(1)
+    print("待機時間0s")
+    #test_turning(10 * i)
 driver.cleanup()
