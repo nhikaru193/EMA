3771343fc3724139d690b072f96cdb9b51283639
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-09 14:13:13 +0900

    Update test_motor_distance.py

--- test_motor_distance.py
+++ test_motor_distance.py
@@ -102,11 +102,11 @@ if err != 0:
 
 print(f"▶ ソフトUART RX を開始：GPIO={RX_PIN}, {BAUD}bps")
 """
-#for i in range (1, 10):
+for i in range (1, 10):
 #following.follow_petit_forward(driver, bno, 90, 0.5)
 #following.follow_petit_forward(driver, bno, 90, 0.5)
-driver.petit_forward(0, 90)
-driver.petit_forward(90, 0)
+    driver.petit_forward(0, 90)
+    driver.petit_forward(90, 0)
 #driver.petit_right(0, 90)
 #driver.petit_right(90, 0)
 #time.sleep(3.0)
