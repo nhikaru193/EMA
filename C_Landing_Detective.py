1f8db3fb969404d51ef634ae164673ffbb90d49c
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-12 08:07:01 +0900

    Update C_Landing_Detective.py

--- C_Landing_Detective.py
+++ C_Landing_Detective.py
@@ -1,1 +1,11 @@
+from BNO055 import BNO055
+import BME280
+
+class Landing:
+    def __init__(bno: BNO055, counter, timeout):
+        self.bno = bno
+        self.counter = counter
+def run(self):
+    print("着地判定を開始します")
+    
 
