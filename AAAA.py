f5f0c59ed1e51bd4b9f68484dc95c6fc97c3ff25
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-12 19:47:48 +0900

    Update AAAA.py

--- AAAA.py
+++ AAAA.py
@@ -23,12 +23,14 @@ from Flag_Detector2 import FlagDetector
 import pigpio
 import busio
 
+"""
 #モータの初期化
 driver = MotorDriver(
     PWMA=12, AIN1=23, AIN2=18,   # 左モーター用（モータA）
     PWMB=19, BIN1=16, BIN2=26,   # 右モーター用（モータB）
     STBY=21                      # STBYピン
 )
+"""
 
 #BNO055の初期設定
 bno = BNO055()
@@ -37,14 +39,14 @@ time.sleep(1)
 bno.setMode(BNO055.OPERATION_MODE_NDOF)
 time.sleep(1)
 bno.setExternalCrystalUse(True)
-
+"""
 # カメラ初期化と設定
 picam2 = Picamera2()
 config = picam2.create_still_configuration(main={"size": (320, 240)})
 picam2.configure(config)
 picam2.start()
 time.sleep(1)
-
+"""
 #関数のインスタンス作成
 RELEASE = Release(bno) #ok
 RELEASE.run()
