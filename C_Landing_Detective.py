524e680d20a132bf6fba022b6c52bd2dab6c10e7
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-12 09:43:23 +0900

    Update C_Landing_Detective.py

--- C_Landing_Detective.py
+++ C_Landing_Detective.py
@@ -1,11 +1,13 @@
 from BNO055 import BNO055
 import BME280
 import time
+import fusing
 
 class Landing:
-    def __init__(bno: BNO055, counter, timeout, p_threshold = 0.20, h_threshold = 0.10):
+    def __init__(bno: BNO055, p_counter = 3, h_counter = 3, timeout = 120, p_threshold = 0.20, h_threshold = 0.10):
         self.bno = bno
-        self.counter = counter
+        self.p_counter = p_counter
+        self.h_counter = h_counter
         self.timeout = timeout
         self.p_threshold = p_threshold
         self.h_threshold = h_threshold
@@ -15,12 +17,45 @@ class Landing:
         print("着地判定を開始します")
         print("方位角変化量:第1シーケンス")
         self.start_time = time.time()
+        max_counter =self.h_counter
+        #heading着地判定
         while True:
             current_time = time.time()
             before_heading = self.bno.getVector(BNO055.VECTOR_EULER)[0]
-            print(f"{before_heading}")
+            print(f"t = {current_time}||heading = {before_heading}")
             time.sleep(1)
             after_heading = self.bno.getVector(BNO055.VECTOR_EULER)[0]
             delta_heading = after_heading - before_heading
-            if delta_heading < self.h_threshold:
-                print("方位角変化量による着地判定の終了")
+            if delta_heading > self.h_threshold:
+                self.h_counter = self.h_counter - 1
+                print(f"方位角着地判定{self.h_counter}回成功！")
+                if self.h_counter == 0:
+                    print("方位角:変化量による着地判定")
+                    break
+            else:
+                self.h_counter = max_counter
+            delta_time = current_time - self.start_time
+            if delta_time > self.timeout:
+                print("方位角:timeoutによる着地判定")
+
+        #環境センサの初期設定
+        
+        #気圧着地判定
+        while True:
+            current_time = time.time()
+            before_pressure = BME280.
+            print(f"t = {current_time}||heading = {before_heading}")
+            time.sleep(1)
+            after_heading = self.bno.getVector(BNO055.VECTOR_EULER)[0]
+            delta_heading = after_heading - before_heading
+            if delta_heading > self.h_threshold:
+                self.h_counter = self.h_counter - 1
+                print(f"方位角着地判定{self.h_counter}回成功！")
+                if self.h_counter == 0:
+                    print("方位角:変化量による着地判定")
+                    break
+            else:
+                self.h_counter = max_counter
+            delta_time = current_time - self.start_time
+            if delta_time > self.timeout:
+                print("方位角:timeoutによる着地判定")
