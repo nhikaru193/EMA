19f75a8e3c2167a757ac2006039b69cd891e3f41
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-12 10:00:35 +0900

    Update C_Landing_Detective.py

--- C_Landing_Detective.py
+++ C_Landing_Detective.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 import struct
 
 class Landing:
-    def __init__(bno: BNO055, p_counter = 3, h_counter = 3, timeout = 40, p_threshold = 0.20, h_threshold = 0.10):
+    def __init__(self, bno: BNO055, p_counter = 3, h_counter = 3, timeout = 40, p_threshold = 0.20, h_threshold = 0.10):
         self.bno = bno
         self.p_counter = p_counter
         self.h_counter = h_counter
@@ -40,9 +40,10 @@ class Landing:
             delta_time = current_time - self.start_time
             if delta_time > self.timeout:
                 print("方位角:timeoutによる着地判定")
+                break
 
         #環境センサの初期設定
-        BME280.inin_bme280()
+        BME280.init_bme280()
         BME280.read_compensate()
         print("気圧変化量:第2シーケンス")
         self.start_time = time.time()
@@ -68,6 +69,7 @@ class Landing:
             delta_time = current_time - self.start_time
             if delta_time > self.timeout:
                 print("気圧:timeoutによる着地判定")
+                break
 
         #溶断回路作動
         print("着地判定正常終了。テグス溶断シーケンスに入ります")
