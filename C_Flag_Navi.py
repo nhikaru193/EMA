95aae4498939e5f2d8245c5136179e120b175b3b
nhikaru193 <nakagawa@kimura-lab.net>
2025-07-12 20:45:21 +0900

    Update C_Flag_Navi.py

--- C_Flag_Navi.py
+++ C_Flag_Navi.py
@@ -184,7 +184,9 @@ class FlagNavigator:
 if __name__ == '__main__':
     # --- 各クラスのインスタンスを作成 ---
     # 1. モータードライバの初期化
-    driver = MotorDriver()
+    driver = MotorDriver(PWMA=12, AIN1=23, AIN2=18,   # 左モーター用（モータA）
+    PWMB=19, BIN1=16, BIN2=26,   # 右モーター用（モータB）
+    STBY=21   )
 
     # 2. BNO055（9軸センサー）の初期化
     try:
