6d76edb918ee992346e3461e97866d85bb9a09bb
nhikaru193 <nakagawa@kimura-lab.net>
2025-07-14 17:51:05 +0900

    Update C_Flag_Navi.py

--- C_Flag_Navi.py
+++ C_Flag_Navi.py
@@ -17,11 +17,11 @@ class FN:
         ロボットのハードウェアと設定を初期化します。
         """
         # --- 設定値 ---
-        self.TARGET_SHAPES = ["三角形"] #, "長方形", "T字", "十字"を追加する
+        self.TARGET_SHAPES = ["三角形", "長方形"] #, "T字", "十字"を追加する
         self.AREA_THRESHOLD_PERCENT = 25.0
         self.turn_speed = 60
-        self.flag_lat = 35.920324666
-        self.flag_lon = 139.9112945
+        #self.flag_lat = 35.920324666
+        #self.flag_lon = 139.9112945
 
         # --- 初期化処理 ---
         self.detector = FlagDetector()
