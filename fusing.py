fcee7c177ce77ae21d362cfc2ecc601c2f1365ce
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-15 15:44:57 +0900

    Update fusing.py

--- fusing.py
+++ fusing.py
@@ -5,7 +5,7 @@ import struct
 def circuit():
     try:
         NICHROME_PIN = 25
-        HEATING_TIME = 5
+        HEATING_TIME = 0.4
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(NICHROME_PIN, GPIO.OUT, initial=GPIO.LOW)
         print("ニクロム線溶断シーケンスを開始します。")
