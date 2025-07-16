8389fa57d4456f7264fdcae01084ef59aa5448a2
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-16 16:12:34 +0900

    Update following.py

--- following.py
+++ following.py
@@ -88,4 +88,23 @@ def follow_petit_forward(driver, bno, base_speed, duration_time):
                 break
     finally:
         print("誘導終了")
+
+if __name__ == "__main__":
+    try:
+        driver = MotorDriver{
+            PWMA=12, AIN1=23, AIN2=18,   # 左モーター用（モータA）
+            PWMB=19, BIN1=16, BIN2=26,   # 右モーター用（モータB）
+            STBY=21                      # STBYピン
+        }
     
+        bno = BNO055()
+        bno.begin()
+        time.sleep(1)
+        bno.setMode(BNO055.OPERATION_MODE_NDOF)
+        time.sleep(1)
+        bno.setExternalCrystalUse(True)
+        
+        follow_forward(driver, bno, 90, 10)
+
+    finally:
+        driver.cleanup()
