23e1cd011cc66ec9f2a383f2dc711727399e0099
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-19 15:21:31 +0900

    Update C_Landing_Detective.py

--- C_Landing_Detective.py
+++ C_Landing_Detective.py
@@ -100,4 +100,5 @@ class LD:
             print("割り込みにより、着地判定をスキップします")
         finally:
             print("着地判定+溶断回路動作の終了です or 強制終了です")
+            time.sleep(5)
             self.driver.cleanup()
