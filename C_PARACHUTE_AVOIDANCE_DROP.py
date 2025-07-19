46459255cea79118e09863a1da93483b675a71ac
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-19 10:12:37 +0900

    Update C_PARACHUTE_AVOIDANCE_DROP.py

--- C_PARACHUTE_AVOIDANCE_DROP.py
+++ C_PARACHUTE_AVOIDANCE_DROP.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     while True:
         sys, gyro, accel, mag = bno.getCalibration()
         print(f"Calib → Sys:{sys}, Gyro:{gyro}, Acc:{accel}, Mag:{mag}", end='\r\n')
-        if gyro == 3 and accel == 3:
+        if gyro == 3:
             print("キャリブレーション完了")
             break
     AVO = PAD(bno)
