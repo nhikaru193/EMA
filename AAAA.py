f0a5c04b8b352f711337f33617f7b0c2afe1a858
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-23 10:48:03 +0900

    Update AAAA.py

--- AAAA.py
+++ AAAA.py
@@ -54,8 +54,8 @@ while True:
 
 
 #関数のインスタンス作成
-#RELEASE = RD(bno) #ok
-#RELEASE.run()
+RELEASE = RD(bno) #ok
+RELEASE.run()
 
 LAND = LD(bno) 
 LAND.run()
@@ -63,31 +63,31 @@ LAND.run()
 AVOIDANCE = PA(bno, goal_location = [35.9175612, 139.9087922]) #ok
 AVOIDANCE.run()
 
-#GPS_StoF = GPS(bno, goal_location = [35.9175612, 139.9087922])
-#GPS_StoF.run()
+GPS_StoF = GPS(bno, goal_location = [35.9175612, 139.9087922])
+GPS_StoF.run()
 
-#FLAG = FN(bno)
-#FLAG.run()
+FLAG = FN(bno)
+FLAG.run()
 """
 SERVO = SM(6)
 SERVO.run()
 """
-#SERVO_PIN = 13  # GPIO13を使用
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(SERVO_PIN, GPIO.OUT)
-#pwm = GPIO.PWM(SERVO_PIN, 50)
-#pwm.start(0)
-#print("逆回転（速い）")
-#set_servo_duty(4.0)
-#time.sleep(7)
-#set_servo_duty(12.5)
-#pwm.stop()
-#GPIO.cleanup()
+SERVO_PIN = 13  # GPIO13を使用
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+pwm = GPIO.PWM(SERVO_PIN, 50)
+pwm.start(0)
+print("逆回転（速い）")
+set_servo_duty(4.0)
+time.sleep(7)
+set_servo_duty(12.5)
+pwm.stop()
+GPIO.cleanup()
 
-#GPS_FtoG = GPS(bno, goal_location = [35.9243464,139.9113269])
-#GPS_FtoG.run()
+GPS_FtoG = GPS(bno, goal_location = [35.9243464,139.9113269])
+GPS_FtoG.run()
 
-#GOAL = GDN(bno, 30)
-#GOAL.run()
+GOAL = GDN(bno, 30)
+GOAL.run()
 
-#print("クラス呼び出し完了です")
+print("クラス呼び出し完了です")
