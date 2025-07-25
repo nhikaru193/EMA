7187b07e2f5d1ccc18ed4ce09f63a9ecbba809f1
nhikaru193 <nakagawa@kimura-lab.net>
2025-07-25 17:40:26 +0900

    Update Flag_Detector3.py

--- Flag_Detector3.py
+++ Flag_Detector3.py
@@ -64,7 +64,7 @@ class FlagDetector:
         輪郭から頂点数や重心・垂心の一致度を用いて図形を判別する。
         """
         shape_name = "不明"
-        epsilon = 0.03 * cv2.arcLength(contour, True)
+        epsilon = 0.01 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
         vertices = len(approx)
 
@@ -121,7 +121,7 @@ class FlagDetector:
         # --- 1. 黒い領域を特定 ---
         hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
         lower_black = np.array([0, 0, 0])
-        upper_black = np.array([180, 255, 30]) # この値は環境に応じて調整　60とかにしたら明度上がって、背景の森も読み取ってしまう
+        upper_black = np.array([180, 255, 60]) # この値は環境に応じて調整　60とかにしたら明度上がって、背景の森も読み取ってしまう
         black_mask = cv2.inRange(hsv, lower_black, upper_black)
         kernel = np.ones((5, 5), np.uint8)
         black_mask = cv2.morphologyEx(black_mask, cv2.MORPH_CLOSE, kernel)
