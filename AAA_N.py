1d52a5c7830b77cb665bc148874a911176a78a3e
gitogitRyotaro <morohashi@kimura-lab.net>
2025-07-26 18:14:51 +0900

    Update AAA_N.py

--- AAA_N.py
+++ AAA_N.py
@@ -37,8 +37,8 @@ def set_servo_duty(duty):
     time.sleep(0.5)
 
 #初期設定
-Flag_location = [35.9240956, 139.9113363]
-Goal_location = [35.9241075, 139.9114496]
+Flag_location = [35.9241075, 139.9114496]
+Goal_location = [35.9242006, 139.9114013]
 
 #BNO055の初期設定
 bno = BNO055()
