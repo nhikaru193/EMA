cc2508d2d3a89d3a130b73dec900ed46f1995840
nhikaru193 <nakagawa@kimura-lab.net>
2025-07-30 08:17:19 +0900

    Update Flag_B.py

--- Flag_B.py
+++ Flag_B.py
@@ -19,7 +19,7 @@ class Flag_B:
     CROSS_SHAPE_VERTEX_RANGE = (11, 13)
     CROSS_SHAPE_MAX_SOLIDITY = 0.75
 
-    LOWER_OUTER_AREA_HSV = np.array([0, 0, 33]) # 120より大きくすれば、より白くなる
+    LOWER_OUTER_AREA_HSV = np.array([0, 0, 120]) # 120より大きくすれば、より白くなる
     UPPER_OUTER_AREA_HSV = np.array([255, 50, 255])
     
     # 領域内の「黒い図形」を抽出するための二値化しきい値
