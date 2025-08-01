51f848c17e6c508fc4f919d2414ce9a725283cb4
gitogitRyotaro <morohashi@kimura-lab.net>
2025-08-01 12:29:38 +0900

    Update test_test.py

--- test_test.py
+++ test_test.py
@@ -1,43 +1,5 @@
-import time
-import serial
-from motor import MotorDriver
-import RPi.GPIO as GPIO
-from picamera2 import Picamera2
-import math
-import numpy
-from C_GOAL_DETECTIVE_NOSHIRO import GDN
-import following
-import cv2
-import camera
-from BNO055 import BNO055
-from C_PARACHUTE_AVOIDANCE import PA
-import smbus
-import math
-import pigpio
-import struct
-import following
-import cv2
-import camera
-
-bno = BNO055()
-bno.begin()
-time.sleep(1)
-bno.setMode(BNO055.OPERATION_MODE_NDOF)
-time.sleep(1)
-bno.setExternalCrystalUse(True)
-
-while True:
-    sys, gyro, accel, mag = bno.getCalibration()
-    print(f"gyro:{gyro}")
-    if gyro == 3 and mag == 3:
-        print("BNO055のキャリブレーション終了")
-        break
-      
-AVOIDANCE = PA(bno, goal_location = [35.9242042, 139.9113654]) #ok
-AVOIDANCE.run()
-
-#完璧でしたGDN
-"""
-GDinN = GDN(bno)
-GDinN.run()
-"""
+# my_script.py
+print("これは最初の行です。")
+print("これは二番目の行です。")
+import datetime
+print(f"現在時刻: {datetime.datetime.now()}")
