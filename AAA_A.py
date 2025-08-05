941efeddacf3c62f91d07eeb864d44c012da6f73
garigari-hota <daoyuanfanang@gmail.com>
2025-08-05 15:48:00 +0900

    Update AAA_A.py

--- AAA_A.py
+++ AAA_A.py
@@ -22,7 +22,7 @@ from C_RELEASE import RD
 from C_Landing_Detective import LD
 from C_PARACHUTE_AVOIDANCE import PA
 from Flag_Navi import FN
-from C_Servo import SM
+#from C_Servo import SM
 from C_excellent_GPS import GPS
 from GDA2 import GDA
 
