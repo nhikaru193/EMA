abc6f8cbf3e279c115413735a29a831b593dbea4
gitogitRyotaro <morohashi@kimura-lab.net>
2025-08-05 14:03:38 +0900

    Update servo_syuunou.py

--- servo_syuunou.py
+++ servo_syuunou.py
@@ -13,31 +13,12 @@ pwm.start(0)
 def set_servo_duty(duty):
     pwm.ChangeDutyCycle(duty)
     time.sleep(0.5)
-
-def changing_servo_reverse(before, after):
-    global speed
-    for i in range (1, 100):
-        delta_speed = (after - before) / 100
-        speed = before + i * delta_speed
-        set_servo_duty(speed)
-        time.sleep(0.1)
     
 try:
-    """
-    print("サーボ停止")
-    set_servo_duty(7.5)
-
-    print("正回転（速い）")
-    changing_servo_reverse(7.5, 10)
-    time.sleep(3)
-    
-    """
     print("収納")
     t = 12.5
     set_servo_duty(t)
     time.sleep(6)
-
-    set
 
 finally:
     pwm.stop()
