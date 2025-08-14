590c54289c63d602f33255cc990b8c12ef224dd9
nhikaru193 <nakagawa@kimura-lab.net>
2025-08-14 12:20:20 +0900

    Update test_stuck.py

--- test_stuck.py
+++ test_stuck.py
@@ -18,5 +18,21 @@ from BNO055 import BNO055
 from motor import MotorDriver
 from Flag_B import Flag_B
 
+#BNO055の初期設定
+bno = BNO055()
+bno.begin()
+time.sleep(1)
+bno.setMode(BNO055.OPERATION_MODE_NDOF)
+time.sleep(1)
+bno.setExternalCrystalUse(True)
+
+while True:
+    sys, gyro, accel, mag = bno.getCalibration()
+    print(f"gyro:{gyro}, mag:{mag}")
+    if gyro == 3 and mag == 3:
+        print("BNO055のキャリブレーション終了")
+        break
+    time.sleep(0.3)
+
 goal_location = [40.1426175, 139.9876533]
 stuck.GPS_navigate(goal_location, bno, driver=None, pi=None)
