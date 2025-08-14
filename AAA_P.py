62a78b77eb973eb427bda2bc49d3ad560dfb05ca
gitogitRyotaro <morohashi@kimura-lab.net>
2025-08-14 12:02:14 +0900

    Update AAA_P.py

--- AAA_P.py
+++ AAA_P.py
@@ -15,26 +15,22 @@ import BME280
 import following
 from BNO055 import BNO055
 from motor import MotorDriver
-from Flag_Detector3 import FlagDetector
+from Flag_B import Flag_B
 
 #ミッション部分
 from C_RELEASE import RD
 from C_Landing_Detective import LD
 from C_PARACHUTE_AVOIDANCE import PA
 from Flag_Navi import FN
-#from C_Servo import SM
+import Servo
 from C_excellent_GPS import GPS
-from GDA2 import GDA
+from C_GOAL_DETECTIVE_NOSHIRO import GDN
 
-GPS_FtoG = GPS(bno, goal_location = Goal_location)
-GPS_FtoG.run()
-
-Flag_location = [35.9242012, 139.9114341]
+#初期設定
+Flag_location_a = [40.1425710, 139.9874577]
+Flag_location_b = [40.1426574, 139.9875167]
 Goal_location = [40.1426175, 139.9876533]
-
-def set_servo_duty(duty):
-    pwm.ChangeDutyCycle(duty)
-    time.sleep(0.5)
+t = 1
 
 #BNO055の初期設定
 bno = BNO055()
@@ -46,13 +42,47 @@ bno.setExternalCrystalUse(True)
 
 while True:
     sys, gyro, accel, mag = bno.getCalibration()
-    print(f"gyro:{gyro}")
+    print(f"gyro:{gyro}, mag:{mag}")
     if gyro == 3 and mag == 3:
         print("BNO055のキャリブレーション終了")
         break
+    time.sleep(0.3)
+
+#ここのタイムスリープは収納待ちのタイムスリープ
+time.sleep(t)
+
+"""
+RELEASE = RD(bno)
+RELEASE.run()
+
+LAND = LD(bno) 
+LAND.run()
+
+time.sleep(3)
+
+print("パラシュート回避を始めます")
+time.sleep(1)
+
+AVOIDANCE = PA(bno, goal_location = Flag_location_a) #ok
+AVOIDANCE.run()
+
+
+GPS_StoE = GPS(bno, goal_location = Flag_location_a)
+GPS_StoE.run()
+
+GPS_StoF = GPS(bno, goal_location = Flag_location_b)
+GPS_StoF.run()
+
+FLAG = FN(bno, flag_location = Flag_location_b) 
+FLAG.run()
+
+Servo.release()
+"""
 
 GPS_FtoG = GPS(bno, goal_location = Goal_location)
 GPS_FtoG.run()
 
-GOAL = GDA(bno, 30)
+GOAL = GDN(bno, 30)
 GOAL.run()
+
+print("Mission Complete")
