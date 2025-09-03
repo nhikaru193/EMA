3409b9908bcb526191d2e8fd8c8f46347e300f9c
garigari-hota <daoyuanfanang@gmail.com>
2025-09-03 15:22:29 +0900

    Update C_Flag_Navi.py

--- C_Flag_Navi.py
+++ C_Flag_Navi.py
@@ -206,7 +206,7 @@ class FN:
                             """
                             if self.detector.last_image is not None:
                             # 1. 保存先のフォルダ名を決める
-                                save_folder = '/home/EM/saved_images'
+                                save_folder = '/home/EMA/saved_images'
                                 
                                 # 2. フォルダが存在しなければ作成する
                                 os.makedirs(save_folder, exist_ok=True)
