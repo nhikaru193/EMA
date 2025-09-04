607250fdcb74f2c8b9ee8c48c8182dd886121f21
garigari-hota <daoyuanfanang@gmail.com>
2025-09-04 10:33:06 +0900

    Update AAA_N.py

--- AAA_N.py
+++ AAA_N.py
@@ -54,7 +54,7 @@ while True:
 #ここのタイムスリープは収納待ちのタイムスリープ
 time.sleep(t)
 
-"""
+
 RELEASE = RD(bno)
 RELEASE.run()
 
@@ -68,7 +68,7 @@ time.sleep(1)
 
 AVOIDANCE = PA(bno, goal_location = Flag_location_a) #ok
 AVOIDANCE.run()
-"""
+
 GPS_StoE = GPS(bno, goal_location = Flag_location_a)
 GPS_StoE.run()
 
