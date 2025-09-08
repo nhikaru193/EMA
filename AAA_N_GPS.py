c8c862d7dd5f4ef695b364afc6a7e7fca51502d6
nhikaru193 <nakagawa@kimura-lab.net>
2025-09-08 09:17:08 +0900

    Update AAA_N_GPS.py

--- AAA_N_GPS.py
+++ AAA_N_GPS.py
@@ -31,7 +31,7 @@ from GDA2 import GDA
 #初期設定
 Flag_location_a = [35.9239009, 139.9125483]
 Flag_location_b = [35.9240070, 139.9126552]
-Goal_location = [35.9239850, 139.9124749]
+Goal_location = [40.8841111, -119.1182222]
 t = 1
 
 #BNO055の初期設定
