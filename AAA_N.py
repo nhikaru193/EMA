17934aee9058001c1cac2df7db4ec3faf714e54c
nhikaru193 <nakagawa@kimura-lab.net>
2025-09-12 02:13:21 +0900

    Update AAA_N.py

--- AAA_N.py
+++ AAA_N.py
@@ -70,21 +70,23 @@ time.sleep(1)
 AVOIDANCE = PA(bno, goal_location = Flag_location_a) #ok
 AVOIDANCE.run()
 
+"""
 GPS_StoE = GPS(bno, goal_location = Flag_location_a)
 GPS_StoE.run()
 
 GPS_StoF = GPS(bno, goal_location = Flag_location_b)
 GPS_StoF.run()
-
+"""
 
 FLAG = FN(bno, flag_location = Flag_location_b) 
 FLAG.run()
 
 Servo.release()
 
-
+"""
 GPS_FtoG = GPS(bno, goal_location = Goal_location)
 GPS_FtoG.run()
+"""
 
 GOAL = GDA(bno, 30)
 GOAL.run()
